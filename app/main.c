768d094664c19e36751d91d43a9811c252398021
Nikita Kozlov <nikita@elyzion.net>
2015-09-22 00:48:44 +0200

    don't decremet ttl/hop_limit for packet sent to the kni

--- app/main.c
+++ app/main.c
@@ -615,11 +615,10 @@ processx4_step_checkneighbor(struct lcore_conf *qconf, struct rte_mbuf **pkt,
 		    process, pkt[j]->ol_flags,                                 \
 		    rte_pktmbuf_mtod(pkt[j], struct ether_hdr *)->ether_type); \
 	}                                                                      \
-	process += ip_process(rte_pktmbuf_mtod_offset(pkt[j], struct ether_hdr *, sizeof(struct ether_hdr)),    \
-			      &dst_port[j], RDPDK_PKT_TYPE(pkt[j]), qconf);    \
+	process += process == 0 ? ip_process(rte_pktmbuf_mtod_offset(pkt[j], struct ether_hdr *, sizeof(struct ether_hdr)),    \
+			      &dst_port[j], RDPDK_PKT_TYPE(pkt[j]), qconf) : 0;    \
 	if (process) {                                                         \
 		/* no dest neighbor addr available, send it through the kni */ \
-		/* FIXME add rate-limit here */                                \
 		knimbuf[i++] = pkt[j];                                         \
 		if (j != --nb_rx) {                                            \
 			/* we have more packets, deplace last one and its info \
