f3fb8e7fbf27014a5cc9444d17dd487e8ae1007e
Nikita Kozlov <nikita@elyzion.net>
2015-09-22 13:58:25 +0200

    fix qemu numa settings

--- tests/integration/lab.sh
+++ tests/integration/lab.sh
@@ -65,7 +65,7 @@ start_vm() {
     IFS="$saveifs"
 
     screen -dmS $name \
-        $(which qemu-system-x86_64) -enable-kvm -cpu host -smp 4 \
+        $(which qemu-system-x86_64) -enable-kvm -cpu host -smp cores=5,sockets=1 \
         -nodefconfig -no-user-config -nodefaults \
         -m 800 \
         -display none \
