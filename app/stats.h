fc85408e172e9d345d1aec298544dc9366551782
Ismael Tifous <ismael.tifous@gandi.net>
2016-06-05 16:56:58 +0200

    clang format

--- app/stats.h
+++ app/stats.h
@@ -33,16 +33,18 @@
 
 enum { CMD_STATS_JSON = 1, CMD_STATS_CSV, CMD_LPM_STATS, CMD_LPM_STATS_JSON };
 
-void nic_stats_display(struct cmdline *cl, portid_t port_id, int option);
-void nic_stats_clear(struct cmdline *cl, portid_t port_id);
-void nic_xstats_display(struct cmdline *cl, portid_t port_id, int option);
-void nic_xstats_clear(struct cmdline *cl, portid_t port_id);
-void port_infos_display(struct cmdline *cl, portid_t port_id);
-void pktj_stats_display(struct cmdline *cl, int option, int delay);
-void pktj_lpm_stats_display(struct cmdline *cl, int is_ipv4, int option);
-void port_rss_hash_conf_show(struct cmdline *cl, portid_t port_id,
+void nic_stats_display(struct cmdline* cl, portid_t port_id, int option);
+void nic_stats_clear(struct cmdline* cl, portid_t port_id);
+void nic_xstats_display(struct cmdline* cl, portid_t port_id, int option);
+void nic_xstats_clear(struct cmdline* cl, portid_t port_id);
+void port_infos_display(struct cmdline* cl, portid_t port_id);
+void pktj_stats_display(struct cmdline* cl, int option, int delay);
+void pktj_lpm_stats_display(struct cmdline* cl, int is_ipv4, int option);
+void port_rss_hash_conf_show(struct cmdline* cl,
+			     portid_t port_id,
 			     int show_rss_key);
-void port_rss_hash_key_update(struct cmdline *cl, portid_t port_id,
-			      uint8_t *hash_key);
+void port_rss_hash_key_update(struct cmdline* cl,
+			      portid_t port_id,
+			      uint8_t* hash_key);
 
 #endif
--- include/pktj_common.h
+++ include/pktj_common.h
@@ -4,11 +4,11 @@
 #define USE_RTE_FUNCS 1
 
 #ifndef USE_RTE_FUNCS
-#define pktj_init(argc, argv)                                                  \
-	(void)(argc);                                                          \
+#define pktj_init(argc, argv) \
+	(void)(argc);         \
 	(void)(argv)
 #define pktj_malloc(str, len, align, socket) malloc(len)
-#define pktj_calloc(str, nb_item, item_len, align, socket)                     \
+#define pktj_calloc(str, nb_item, item_len, align, socket) \
 	calloc((nb_item), (item_len))
 #define pktj_free(ptr) free(ptr)
 #define pktj_unused(x) x __attribute__((unused))
@@ -17,9 +17,9 @@
 #include <rte_common.h>
 #include <rte_malloc.h>
 #define pktj_init(argc, argv) rte_eal_init(argc, argv)
-#define pktj_malloc(str, len, flag, socket)                                    \
+#define pktj_malloc(str, len, flag, socket) \
 	rte_malloc_socket(str, (len), flag, socket)
-#define pktj_calloc(str, nb_item, item_len, flag, socket)                      \
+#define pktj_calloc(str, nb_item, item_len, flag, socket) \
 	rte_calloc_socket(str, (nb_item), (item_len), flag, socket)
 #define pktj_free(ptr) rte_free(ptr)
 #define pktj_unused(x) __rte_unused x
